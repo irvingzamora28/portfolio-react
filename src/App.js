8206a76dc295a8218dc4ca758df9df70a7551e0e
Irving Zamora <irvingzamora28@gmail.com>
2021-05-13 20:14:39 -0600

    Portfolio reset

--- src/App.js
+++ src/App.js
@@ -1,21 +1,7 @@
-import About from "./components/About"
-import AboutBottom from "./components/AboutBottom"
-import Articles from "./components/Articles"
-import Footer from "./components/Footer"
-import Grid from "./components/Grid"
-import Header from "./components/Header"
-import Showcase from "./components/Showcase"
 
 const App = () => {
 	return (
 		<>
-		<Header />
-		<Showcase />
-		<About />
-		<Grid />
-		<AboutBottom />
-		<Articles />
-		<Footer />
 		</>
   	);
 }
