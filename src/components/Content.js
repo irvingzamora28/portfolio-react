db4a7b7eb118013604a32afbff030c0d193abb7d
Irving Zamora <irvingzamora28@gmail.com>
2022-02-03 18:13:50 -0700

    Add CopyBlock to Content component

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { CopyBlock, dracula } from 'react-code-blocks';
 import ReactDOM from 'react-dom';
 
 const Content = (props) => {
@@ -7,13 +8,26 @@ const Content = (props) => {
     const children = data.map((elem, index) => {
         console.log("elem");
         console.log(elem.text);
-        return React.createElement(
-            elem.element,
-            { key: index, className: 'greeting', id: elem.id, src: elem.src },
-            elem.text
-        );
+        if (elem.element === "code") {
+            return <p className="py-2">
+                <CopyBlock
+                    text={elem.text}
+                    language={elem.language}
+                    showLineNumbers={true}
+                    startingLineNumber={1}
+                    wrapLines
+                    theme={dracula}
+                />
+            </p>
+        } else {
+            return React.createElement(
+                elem.element,
+                { key: index, className: 'py-2', id: elem.id, src: elem.src },
+                elem.text
+            );
+        }
     });
- 
+
     return (
         <>
             {children}
